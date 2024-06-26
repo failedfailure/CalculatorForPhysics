48c6ae0b3479c65f37617aa87ab595ad69c02fef
jdh <fa1lure_jdh@outlook.com>
2024-06-26 23:29:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ import math
 
 print("適当な小道具です! \n")
 type = input("円柱? (1) 円筒? (2) ")
-# 二つの種類の物を区分
+def correct(list_corr):
+    status = input("修正する(Y/N)? ")
+    if (status == "Y") or (status == "y"):
+        num_to_corr = int(input("何個目を修正? ")) - 1
+        list_corr[num_to_corr] = float(input("何に修正? "))
+        correct(list_corr)
+
+
 if int(type) == 1:
     list_M = []
     list_D = []
     list_L = []
-    print("質量を5回いれて")
+    print("\n質量を5回いれて")
     for i in range(0,5):
         list_M.append(float(input()))
-    print("直径を5回いれて")
+    correct(list_M)
+    print("\n直径を5回いれて")
     for i in range(0,5):
         list_D.append(float(input()))
-    print("長さを5回いれて")
+    correct(list_D)
+    print("\n長さを5回いれて")
     for i in range(0,5):
         list_L.append(float(input()))
+    correct(list_L)
     sum_M = 0.0
     sum_D = 0.0
     sum_L = 0.0
@@ -39,33 +49,39 @@ if int(type) == 1:
     averdelta_M = delta_M / math.sqrt(5)
     averdelta_D = delta_D / math.sqrt(5)
     averdelta_L = delta_L / math.sqrt(5)
-    print("質量の平均值は" + str(aver_M) + "\n質量の誤差は" + str(averdelta_M))
-    print("直径の平均值は" + str(aver_D) + "\n直径の誤差は" + str(averdelta_D))
-    print("長さの平均值は" + str(aver_L) + "\n長さの誤差は" + str(averdelta_L))
+    print("\n---結果---")
+    print("\n質量の平均值は" + str(aver_M) + "\n質量の誤差は" + str(averdelta_M))
+    print("\n直径の平均值は" + str(aver_D) + "\n直径の誤差は" + str(averdelta_D))
+    print("\n長さの平均值は" + str(aver_L) + "\n長さの誤差は" + str(averdelta_L))
     aver_V = math.pi * (aver_D / 2) * (aver_D / 2) * aver_L / 1000
     averdelta_V = aver_V * math.sqrt((2 * averdelta_D / aver_D) * (2 * averdelta_D / aver_D) + (averdelta_L / aver_L) * (averdelta_L / aver_L))
     aver_dens = aver_M / aver_V
     averdelta_dens = aver_dens * math.sqrt((2 * averdelta_D / aver_D) * (2 * averdelta_D / aver_D) + (averdelta_L / aver_L) * (averdelta_L / aver_L) + (averdelta_M / aver_M) * (averdelta_M / aver_M))
-    print("体積は" + str(aver_V) + "\n体積の誤差は" + str(averdelta_V))
-    print("密度は" + str(aver_dens) + "\n密度の誤差は" + str(averdelta_dens))
+    print("\n体積は" + str(aver_V) + "\n体積の誤差は" + str(averdelta_V))
+    print("\n密度は" + str(aver_dens) + "\n密度の誤差は" + str(averdelta_dens))
+    print("\n")
 
 elif int(type) == 2:
     list_M = []
     list_D = []
     list_t = []
     list_L = []
-    print("質量を5回いれて")
+    print("\n質量を5回いれて")
     for i in range(0,5):
         list_M.append(float(input()))
-    print("直径を5回いれて")
+    correct(list_M)
+    print("\n直径を5回いれて")
     for i in range(0,5):
         list_D.append(float(input()))
-    print("厚さを5回いれて")
+    correct(list_D)
+    print("\n厚さを5回いれて")
     for i in range(0,5):
         list_t.append(float(input()))
-    print("長さを5回いれて")
+    correct(list_t)
+    print("\n長さを5回いれて")
     for i in range(0,5):
         list_L.append(float(input()))
+    correct(list_L)
     sum_M = 0.0
     sum_D = 0.0
     sum_t = 0.0
@@ -96,16 +112,18 @@ elif int(type) == 2:
     averdelta_D = delta_D / math.sqrt(5)
     averdelta_t = delta_t / math.sqrt(5)
     averdelta_L = delta_L / math.sqrt(5)
-    print("質量の平均值は" + str(aver_M) + "\n質量の誤差は" + str(averdelta_M))
-    print("直径の平均值は" + str(aver_D) + "\n直径の誤差は" + str(averdelta_D))
-    print("厚さの平均值は" + str(aver_t) + "\n厚さの誤差は" + str(averdelta_t))
-    print("長さの平均值は" + str(aver_L) + "\n長さの誤差は" + str(averdelta_L))
+    print("\n---結果---")
+    print("\n質量の平均值は" + str(aver_M) + "\n質量の誤差は" + str(averdelta_M))
+    print("\n直径の平均值は" + str(aver_D) + "\n直径の誤差は" + str(averdelta_D))
+    print("\n厚さの平均值は" + str(aver_t) + "\n厚さの誤差は" + str(averdelta_t))
+    print("\n長さの平均值は" + str(aver_L) + "\n長さの誤差は" + str(averdelta_L))
     aver_V = math.pi * (aver_D - aver_t) * aver_t * aver_L / 1000
     averdelta_V = aver_V * math.sqrt((averdelta_D / aver_D) * (averdelta_D / aver_D) + (averdelta_t / aver_t) * (averdelta_t / aver_t) + (averdelta_L / aver_L) * (averdelta_L / aver_L))
     aver_dens = aver_M / aver_V
     averdelta_dens = aver_dens * math.sqrt((averdelta_D / aver_D) * (averdelta_D / aver_D) + (averdelta_t / aver_t) * (averdelta_t / aver_t) + (averdelta_L / aver_L) * (averdelta_L / aver_L) + (averdelta_M / aver_M) * (averdelta_M / aver_M))
-    print("体積は" + str(aver_V) + "\n体積の誤差は" + str(averdelta_V))
-    print("密度は" + str(aver_dens) + "\n密度の誤差は" + str(averdelta_dens))
+    print("\n体積は" + str(aver_V) + "\n体積の誤差は" + str(averdelta_V))
+    print("\n密度は" + str(aver_dens) + "\n密度の誤差は" + str(averdelta_dens))
+    print("\n")
 
 else:
     print("Nah")
